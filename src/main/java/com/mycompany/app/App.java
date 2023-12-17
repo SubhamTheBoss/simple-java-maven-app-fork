3292c0e4a6b8243152f0bc2e208dc5b03cd7f80f
SubhamTheBoss <tonystarksub@gmail.com>
2023-12-18 03:00:32 +0530

    Update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,12 +7,12 @@ public class App {
 
 	private static final String MESSAGE = "Hello World!";
 
-	public App() {
-		// this constructor is empty
-	}
-
 	public static void main(String[] args) {
 		System.out.println(MESSAGE);
+	}
+
+	public App() {
+		// this constructor is empty
 	}
 
 	public String getMessage() {
